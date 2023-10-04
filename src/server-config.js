ce1a5ab373877c69884fa364de3107424254ae12
sahanilxm <laxmisahani240@gmail.com>
2023-10-05 01:48:18 +0530

    ??.

--- src/server-config.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require('dotenv');
-
-dotenv.config();
-
-module.exports = {
-    PORT : process.env.PORT,
-    SWIGGY_RESTAURANT_API : process.env.SWIGGY_RESTAURANT_API,
-    SWIGGY_RESTAURANT_MENU_API: process.env.SWIGGY_RESTAURANT_MENU_API
-}
